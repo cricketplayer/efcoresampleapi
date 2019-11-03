8a7cb70592cc91fa52c5fe5e8c754a81f31dcb5e
Ankur Lalit <ankur.lalit@servcorp.com.au>
2019-11-04 08:46:35 +1100

    Added a dbcontext

---
+++ EFCoreSampleApi.Infrastructure/PersonContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace EFCoreSampleApi.Infrastructure
+{
+    public class PersonContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlServer(@"Server=.\SQLEXPRESS;Database=PersonDB;Trusted_Connection=True;");
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Blog> Blogs { get; set; }
+    }
+    public class Blog
+    {
+        public int PersonId { get; set; }
+        public int BlogId { get; set; }
+        public string Url { get; set; }
+
+        public List<Post> Posts { get; } = new List<Post>();
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public int BlogId { get; set; }
+        public Blog Blog { get; set; }
+    }
+}
